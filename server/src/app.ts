8fa90aebf4e3851371db84b115779d67d7c749c9
Klaudiusz Dembler <k.dembler@gmail.com>
2018-06-11 19:28:58 +0200

    Add node backend

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import * as express from 'express'
+import * as path from 'path'
+
+const clientBuildPath = path.join(__dirname, '..', '..', 'build')
+const port = process.env.NODE_ENV === 'development' ? 3001 : 8080
+
+const app = express()
+app.use(express.static(clientBuildPath))
+
+app.get('/', (req, res) => res.sendFile(path.join(clientBuildPath, 'index.html')))
+app.get('/api', (req, res) => res.send({ msg: 'Hello from API!' }))
+
+app.listen(port, () => console.log(`Booker listening on ${port}!`))
