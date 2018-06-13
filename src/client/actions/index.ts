3607eda85ef6efbbfe4686fecc0925fb932281a1
Klaudiusz Dembler <k.dembler@gmail.com>
2018-06-13 14:22:41 +0200

    Remove delays

--- src/client/actions/index.ts
+++ src/client/actions/index.ts
@@ -5,84 +5,74 @@ import { Book, BookerState } from '../types'
 import { apiRequest } from '../utils'
 import * as types from './types'
 
-const delay = (s: number) => new Promise((resolve, error) => setTimeout(() => resolve(), s * 1000))
-
 type AsyncAction = ThunkAction<Promise<void>, BookerState, undefined, types.BookerAction>
 
 // async actions
 export const refreshBooks = (): AsyncAction => {
   return (dispatch: Dispatch<types.BookerAction>) => {
-    return delay(1).then(() => {
-      return fetch('/api/books')
-        .then(response => {
-          if (!response.ok) {
-            throw null
-          }
-          return response.json()
+    return fetch('/api/books')
+      .then(response => {
+        if (!response.ok) {
+          throw null
+        }
+        return response.json()
+      })
+      .then(json => {
+        const books = json as Book[]
+        dispatch({
+          books,
+          type: 'BOOKS_REPLACE_ALL'
         })
-        .then(json => {
-          const books = json as Book[]
-          dispatch({
-            books,
-            type: 'BOOKS_REPLACE_ALL'
-          })
-        })
-    })
+      })
   }
 }
 
 export const addBook = (book: Book): AsyncAction => {
   return (dispatch: Dispatch<types.BookerAction>) => {
-    return delay(1).then(() => {
-      const data = JSON.stringify(book)
-      return apiRequest('POST', data).then(({ response, status }) => {
-        if (status === 201) {
-          dispatch({
-            book,
-            type: 'BOOK_ADD'
-          })
-        } else {
-          throw { status }
-        }
-      })
+    const data = JSON.stringify(book)
+    return apiRequest('POST', data).then(({ response, status }) => {
+      if (status === 201) {
+        dispatch({
+          book,
+          type: 'BOOK_ADD'
+        })
+      } else {
+        throw { status }
+      }
     })
   }
 }
 
 export const editBook = (book: Book, isbn: string): AsyncAction => {
   return (dispatch: Dispatch<types.BookerAction>) => {
-    return delay(1).then(() => {
-      const data = JSON.stringify(book)
-      return apiRequest('PUT', data, `/${isbn}`).then(({ response, status }) => {
-        if (status === 204) {
-          dispatch({
-            book,
-            isbn,
-            type: 'BOOK_EDIT'
-          })
-        } else {
-          throw null
-        }
-      })
+    const data = JSON.stringify(book)
+    return apiRequest('PUT', data, `/${isbn}`).then(({ response, status }) => {
+      if (status === 204) {
+        dispatch({
+          book,
+          isbn,
+          type: 'BOOK_EDIT'
+        })
+      } else {
+        throw null
+      }
     })
   }
 }
 
 export const removeBook = (isbn: string): AsyncAction => {
   return (dispatch: Dispatch<types.BookerAction>) => {
-    return delay(1).then(() => {
-      const data = undefined
-      return apiRequest('DELETE', data, `/${isbn}`).then(({ response, status }) => {
-        if (status === 204 || status === 404) {
-          // accept 404 as well, user doesn't care that it was already deleted
-          dispatch({
-            isbn,
-            type: 'BOOK_REMOVE'
-          })
-        } else {
-          throw null
-        }
-      })
+    const data = undefined
+    return apiRequest('DELETE', data, `/${isbn}`).then(({ response, status }) => {
+      if (status === 204 || status === 404) {
+        // accept 404 as well, user doesn't care that it was already deleted
+        dispatch({
+          isbn,
+          type: 'BOOK_REMOVE'
+        })
+      } else {
+        throw null
+      }
     })
   }
 }
